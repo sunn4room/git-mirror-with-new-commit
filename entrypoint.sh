cd4372f34a5ff64db9add4e5f73dd749bc6ccdbb
sunn4room <sunn4room@163.com>
2023-12-10 11:28:36 +0800

    dummy run

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,25 +15,25 @@ echo "$INPUT_SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 echo "StrictHostKeyChecking no" > ~/.ssh/config
 
-GIT_CLONE_SOURCE="git clone --depth 1"
-test -n "$INPUT_SOURCE_BRANCH" && GIT_CLONE_SOURCE="$GIT_CLONE_SOURCE --branch $INPUT_SOURCE_BRANCH"
-GIT_CLONE_SOURCE="$GIT_CLONE_SOURCE git@$INPUT_SOURCE_REPO.git $HOME/source-repo"
-echo "$GIT_CLONE_SOURCE"
-$GIT_CLONE_SOURCE || error "clone source repo failed"
+# GIT_CLONE_SOURCE="git clone --depth 1"
+# test -n "$INPUT_SOURCE_BRANCH" && GIT_CLONE_SOURCE="$GIT_CLONE_SOURCE --branch $INPUT_SOURCE_BRANCH"
+# GIT_CLONE_SOURCE="$GIT_CLONE_SOURCE git@$INPUT_SOURCE_REPO.git $HOME/source-repo"
+# echo "$GIT_CLONE_SOURCE"
+# $GIT_CLONE_SOURCE || error "clone source repo failed"
 
 GIT_CLONE_DESTINATION="git clone --depth 1"
 test -n "$INPUT_DESTINATION_BRANCH" && GIT_CLONE_DESTINATION="$GIT_CLONE_DESTINATION --branch $INPUT_DESTINATION_BRANCH"
 GIT_CLONE_DESTINATION="$GIT_CLONE_DESTINATION git@$INPUT_DESTINATION_REPO.git $HOME/destination-repo"
 echo "$GIT_CLONE_DESTINATION"
-$GIT_CLONE_DESTINATION >/dev/null 2>&1 || error "clone destination repo failed"
+$GIT_CLONE_DESTINATION || error "clone destination repo failed"
 
-rm -rf ~/source-repo/.git
-cp -r ~/destination-repo/.git ~/source-repo/.git
-cd ~/source-repo
-if test "$(git status -s | wc -l)" -eq "0"; then
-  echo "there is no change"
-  exit 0
-fi
-git add --all >/dev/null 2>&1 || error "git add failed"
-git commit -m "$(date)" >/dev/null 2>&1 || error "git commit failed"
-git push >/dev/null 2>&1 || error "git push failed"
+# rm -rf ~/source-repo/.git
+# cp -r ~/destination-repo/.git ~/source-repo/.git
+# cd ~/source-repo
+# if test "$(git status -s | wc -l)" -eq "0"; then
+#   echo "there is no change"
+#   exit 0
+# fi
+# git add --all >/dev/null 2>&1 || error "git add failed"
+# git commit -m "$(date)" >/dev/null 2>&1 || error "git commit failed"
+# git push >/dev/null 2>&1 || error "git push failed"
