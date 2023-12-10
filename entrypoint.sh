85e212636425ec1722ee8cd8e64e2e573b9d2e1a
sunn4room <sunn4room@163.com>
2023-12-10 12:17:35 +0800

    dummy run

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,18 +19,18 @@ test -n "$INPUT_GIT_USER_EMAIL" || error "git user email is null"
 mkdir -p /root/.ssh
 echo "StrictHostKeyChecking no" > /etc/ssh/ssh_config
 
+echo "clone source repo"
 test -n "$INPUT_SOURCE_KEY" && set_key "$INPUT_SOURCE_KEY"
 GIT_CLONE_SOURCE="git clone --depth 1"
 test -n "$INPUT_SOURCE_BRANCH" && GIT_CLONE_SOURCE="$GIT_CLONE_SOURCE --branch $INPUT_SOURCE_BRANCH"
 GIT_CLONE_SOURCE="$GIT_CLONE_SOURCE $INPUT_SOURCE_REPO $HOME/source-repo"
-echo "$GIT_CLONE_SOURCE"
 $GIT_CLONE_SOURCE >/dev/null 2>&1 || error "clone source repo failed"
 
+echo "clone destination repo"
 set_key "$INPUT_DESTINATION_KEY"
 GIT_CLONE_DESTINATION="git clone --depth 1"
 test -n "$INPUT_DESTINATION_BRANCH" && GIT_CLONE_DESTINATION="$GIT_CLONE_DESTINATION --branch $INPUT_DESTINATION_BRANCH"
 GIT_CLONE_DESTINATION="$GIT_CLONE_DESTINATION $INPUT_DESTINATION_REPO $HOME/destination-repo"
-echo "$GIT_CLONE_DESTINATION"
 $GIT_CLONE_DESTINATION >/dev/null 2>&1 || error "clone destination repo failed"
 
 rm -rf ~/source-repo/.git
@@ -40,8 +40,11 @@ if test "$(git status -s | wc -l)" -eq "0"; then
   echo "there is no change"
   exit 0
 fi
+
+echo "push changes"
 git config --global user.name "$INPUT_GIT_USER_NAME"
 git config --global user.email "$INPUT_GIT_USER_EMAIL"
 git add --all >/dev/null 2>&1 || error "git add failed"
 git commit -m "$(date)" >/dev/null 2>&1 || error "git commit failed"
 git push >/dev/null 2>&1 || error "git push failed"
+echo "git mirror done"
